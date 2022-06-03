fc38b93b4a3145c3593152535df292f48a22fd41
sincanmaulanaa <sincanmaulanaa@gmail.com>
2022-06-03 22:56:25 +0700

    feat: add back-to-top feature

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,4 @@
 //Menampilkan/menyembunyikan layanan lain
-
 const moreServices = document.getElementById("more-services");
 const moreBtn = document.getElementById("more");
 
@@ -11,7 +10,6 @@ moreBtn.addEventListener("click", function () {
 });
 
 //Melakukan perulangan untuk menampilkan bintang disetiap testimonial item
-
 const testimonialStar = document.querySelectorAll(".testimonial-star");
 console.log(testimonialStar);
 for (let i = 0; i < testimonialStar.length; i++) {
@@ -19,3 +17,49 @@ for (let i = 0; i < testimonialStar.length; i++) {
 
   star.innerText = "⭐⭐⭐⭐⭐";
 }
+
+// Mobile menu
+const burgerIcon = document.getElementById("burger-icon");
+const mobileMenu = document.getElementById("nav-menu");
+
+burgerIcon.addEventListener("click", function () {
+  mobileMenu.classList.toggle("hidden");
+  mobileMenu.classList.toggle("display");
+});
+
+// klik diluar mobile menu
+window.addEventListener("click", function (e) {
+  if (e.target != burgerIcon && e.target != mobileMenu) {
+    mobileMenu.classList.add("hidden");
+    mobileMenu.classList.remove("display");
+  }
+});
+
+// Back to top
+window.onscroll = function () {
+  const header = document.querySelector("header");
+  const fixedNav = header.offsetTop;
+  const backToTop = document.querySelector(".to-top");
+
+  if (window.pageYOffset > fixedNav) {
+    backToTop.classList.add("back-to-top");
+  } else {
+    backToTop.classList.remove("back-to-top");
+  }
+};
+
+// const burger = document.querySelector("#burger");
+// const navMenu = document.querySelector("#nav-menu");
+
+// burger.addEventListener("click", function () {
+//   burger.classList.toggle("burger-active");
+//   navMenu.classList.toggle("hidden");
+// });
+
+// // Klik diluar burger
+// window.addEventListener("click", function (e) {
+//   if (e.target != burger && e.target != navMenu) {
+//     burger.classList.remove("burger-active");
+//     navMenu.classList.add("hidden");
+//   }
+// });
